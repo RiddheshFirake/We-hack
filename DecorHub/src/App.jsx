c9e08589050cbc8b46c56bcec830bd3fe53fb7d1
Dhir_Salvi <150221513+DhirSalvi@users.noreply.github.com>
2025-03-22 17:55:38 +0530

    Created HomePage and Dashboard pages

--- DecorHub/src/App.jsx
+++ DecorHub/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Routes, Route } from 'react-router-dom';
 
+import HomePage from './pages/HomePage';
+// import LoginPage from './pages/LoginPage';
+// import SignupPage from './pages/SignupPage';
+import Dashboard from './pages/Dashboard';
+// import Profile from './pages/Profile';
+// import PaintWall from './pages/PaintWall';
+// import PaintCost from './pages/PaintCost';
+// import Generate1 from './pages/Generate1';
+// import Compare from './pages/Compare';
+// import DallEPage from './pages/DallEPage';
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className='px-24'>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/login" element={<LoginPage />} /> */}
+        {/* <Route path="/signup" element={<SignupPage />} /> */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/generate" element={<Generate1 />} /> */}
+        {/* <Route path="/profile" element={<Profile />} /> */}
+        {/* <Route path="/paint" element={<PaintWall />} /> */}
+        {/* <Route path="/paint-cost" element={<PaintCost />} /> */}
+        {/* <Route path='/compare' element={<Compare />} /> */}
+        {/* <Route path='/dallE' element={<DallEPage />} /> */}
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
